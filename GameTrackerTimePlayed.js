06764a41c062a73d4ac41fb579e35f9599c8c328
@LeX <ghebaura34@gmail.com>
2023-05-01 00:14:45 +0300

    Update GameTrackerTimePlayed.js

--- GameTrackerTimePlayed.js
+++ GameTrackerTimePlayed.js
@@ -3,7 +3,8 @@
 // @name:ro          GameTracker Time Played
 // @name:es          Tiempo de juego en GameTracker
 // @name:pl          Czas gry w GameTracker
-// @namespace        http://tampermonkey.net/
+// @namespace        http://www.tampermonkey.net/
+// @homepage         https://greasyfork.org/ro/scripts/465212-gametracker-time-played/
 // @version          1.7
 // @description      Displays the time played in days, hours, and minutes for a player on GameTracker.
 // @description:ro   Afișează timpul jucat în zile, ore și minute pentru un jucător pe GameTracker.
@@ -12,8 +13,6 @@
 // @match            https://www.gametracker.com/player/*
 // @icon             https://www.gametracker.com/images/icons/icon16x16_gt.png
 // @grant            none
-// @updateURL        https://raw.githubusercontent.com/ALeX400/UserScripts/main/GameTrackerTimePlayed.js
-// @downloadURL      https://raw.githubusercontent.com/ALeX400/UserScripts/main/GameTrackerTimePlayed.js
 // @require          https://code.jquery.com/jquery-3.6.0.min.js
 // @source           https://github.com/ALeX400/UserScripts/blob/main/GameTrackerTimePlayed.js
 // @license          MIT
@@ -92,3 +91,4 @@
         return timePlayedString;
     }
 })();
+
