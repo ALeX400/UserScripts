ea9f244cadc30266b479f6140bf64344d7b03b1e
@LeX <ghebaura34@gmail.com>
2023-04-30 23:56:24 +0300

    Update GameTrackerTimePlayed.js

--- GameTrackerTimePlayed.js
+++ GameTrackerTimePlayed.js
@@ -13,11 +13,77 @@
 // @source       https://github.com/ALeX400/UserScripts/blob/main/GameTrackerTimePlayed.js
 // ==/UserScript==
 
-(function(){'use strict';const sectionTitles=document.querySelectorAll('div.section_title');for(const sectionTitle of sectionTitles){if(sectionTitle.textContent.includes("ALL TIME STATS")){const itemColorTitles=sectionTitle.parentElement.querySelectorAll('span.item_color_title');for(const title of itemColorTitles){if(title.textContent.includes("Minutes Played:")){const minutesPlayedText=title.nextSibling;const minutesPlayed=parseInt(minutesPlayedText.textContent,10);const timePlayed=convertMinutesToTimePlayed(minutesPlayed);const timePlayedString=formatTimePlayedString(timePlayed);const separator="\n\t\t\t\t\t\t\t";const totalTimeTitle=document.createElement('span');totalTimeTitle.classList.add('item_color_title');const totalTimeText=document.createTextNode(`${separator}Total Time:${separator}`);totalTimeTitle.appendChild(totalTimeText);const totalTimeNum=document.createTextNode(`${separator}${timePlayedString}${separator}`);title.parentElement.insertBefore(document.createElement('br'),minutesPlayedText.nextSibling);title.parentElement.insertBefore(totalTimeTitle,minutesPlayedText.nextSibling.nextSibling);title.parentElement.insertBefore(totalTimeNum,totalTimeTitle.nextSibling);break}}
-break}}
-function convertMinutesToTimePlayed(minutes){const minutesInHour=60;const hoursInDay=24;const minutesInDay=minutesInHour*hoursInDay;const days=Math.floor(minutes/minutesInDay);const hours=Math.floor((minutes%minutesInDay)/minutesInHour);const remainingMinutes=minutes%minutesInHour;return{days:days,hours:hours,minutes:remainingMinutes}}
-function formatTimePlayedString(timePlayed){let timePlayedString="";let timeComponents=[];if(timePlayed.days>0){timeComponents.push(`${timePlayed.days} ${timePlayed.days === 1 ? 'day' : 'days'}`)}
-if(timePlayed.hours>0){timeComponents.push(`${timePlayed.hours} ${timePlayed.hours === 1 ? 'hour' : 'hours'}`)}
-if(timePlayed.minutes>0){timeComponents.push(`${timePlayed.minutes} ${timePlayed.minutes === 1 ? 'minute' : 'minutes'}`)}
-if(timeComponents.length>1){const lastComponent=timeComponents.pop();timePlayedString=timeComponents.join(', ')+' and '+lastComponent}else{timePlayedString=timeComponents[0]}
-return timePlayedString}})()
+(function () {
+    'use strict';
+    const sectionTitles = document.querySelectorAll('div.section_title');
+    for (const sectionTitle of sectionTitles) {
+        if (sectionTitle.textContent.includes("ALL TIME STATS")) {
+            const itemColorTitles = sectionTitle.parentElement.querySelectorAll('span.item_color_title');
+            for (const title of itemColorTitles) {
+                if (title.textContent.includes("Minutes Played:")) {
+                    const minutesPlayedText = title.nextSibling;
+                    const minutesPlayed = parseInt(minutesPlayedText.textContent, 10);
+                    const timePlayed = convertMinutesToTimePlayed(minutesPlayed);
+                    const timePlayedString = formatTimePlayedString(timePlayed);
+
+
+                    const separator = "\n\t\t\t\t\t\t\t";
+
+                    const totalTimeTitle = document.createElement('span');
+                    totalTimeTitle.classList.add('item_color_title');
+
+                    const totalTimeText = document.createTextNode(`${separator}Total Time:${separator}`);
+                    totalTimeTitle.appendChild(totalTimeText);
+
+                    const totalTimeNum = document.createTextNode(`${separator}${timePlayedString}${separator}`);
+
+                    title.parentElement.insertBefore(document.createElement('br'), minutesPlayedText.nextSibling);
+                    title.parentElement.insertBefore(totalTimeTitle, minutesPlayedText.nextSibling.nextSibling);
+                    title.parentElement.insertBefore(totalTimeNum, totalTimeTitle.nextSibling);
+
+                    break;
+                }
+            }
+            break;
+        }
+    }
+    function convertMinutesToTimePlayed(minutes) {
+        const minutesInHour = 60;
+        const hoursInDay = 24;
+        const minutesInDay = minutesInHour * hoursInDay;
+
+        const days = Math.floor(minutes / minutesInDay);
+        const hours = Math.floor((minutes % minutesInDay) / minutesInHour);
+        const remainingMinutes = minutes % minutesInHour;
+
+        return {
+            days: days,
+            hours: hours,
+            minutes: remainingMinutes
+        };
+    }
+    function formatTimePlayedString(timePlayed) {
+        let timePlayedString = "";
+        let timeComponents = [];
+
+        if (timePlayed.days > 0) {
+            timeComponents.push(`${timePlayed.days} ${timePlayed.days === 1 ? 'day' : 'days'}`);
+        }
+        if (timePlayed.hours > 0) {
+            timeComponents.push(`${timePlayed.hours} ${timePlayed.hours === 1 ? 'hour' : 'hours'}`);
+        }
+        if (timePlayed.minutes > 0) {
+            timeComponents.push(`${timePlayed.minutes} ${timePlayed.minutes === 1 ? 'minute' : 'minutes'}`);
+        }
+
+        if (timeComponents.length > 1) {
+            const lastComponent = timeComponents.pop();
+            timePlayedString = timeComponents.join(', ') + ' and ' + lastComponent;
+        } else {
+            timePlayedString = timeComponents[0];
+        }
+
+        return timePlayedString;
+    }
+})();
+
